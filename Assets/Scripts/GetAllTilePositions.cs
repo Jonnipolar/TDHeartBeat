a3af5f7a7b6ba81e4fdeabf6e21decad87164e08
Jonni <jonj16@ru.is>
2020-03-18 15:14:57 +0000

    update for tilemap scripts and layering for tilemap

---
+++ Assets/Scripts/GetAllTilePositions.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GetAllTilePositions : MonoBehaviour
+{
+
+    public Tilemap tileMap;
+ 
+    public List<Vector3> availablePlaces;
+    public List<Vector3Int> availablePlacesCell;
+ 
+    void Awake () 
+    {
+        tileMap = transform.GetComponentInParent<Tilemap>();
+        availablePlaces = new List<Vector3>();
+ 
+        for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++)
+        {
+            for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++)
+            {
+                Vector3Int cellSpace = (new Vector3Int(n, p, (int)tileMap.transform.position.y));
+                Vector3 place = tileMap.CellToWorld(cellSpace);
+                if (tileMap.HasTile(cellSpace))
+                {
+                    //Tile at "place"
+                    availablePlaces.Add(place);
+                    availablePlacesCell.Add(cellSpace);
+                }
+                else
+                {
+                    //No tile at "place"
+                }
+            }
+        }
+    }
+
+    void Start()
+    {
+        for(var i = 0; i < availablePlaces.Count; i++)
+        {
+            Debug.Log($"World: {availablePlaces[i]} \nCell: {availablePlacesCell[i]}");
+        }
+
+        Debug.Log($"Size of available places: {availablePlaces.Count}");
+        Debug.Log($"Size of available places Cell: {availablePlacesCell.Count}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GetTilePosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class GetTilePosition : MonoBehaviour
+{
+    public Tilemap m_level;
+    Vector3Int oldpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_level = GetComponent<Tilemap>();
+        oldpos = new Vector3Int(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        Vector3Int position = m_level.WorldToCell(worldPoint);
+        TileBase tile = m_level.GetTile(position);
+        Vector3 world2 = m_level.GetCellCenterWorld(position);
+        if(tile && position != oldpos) {
+            
+            // Debug.Log("World position of mouse: "+ worldPoint);
+            // Debug.Log("Cell space position: "+ position);
+            // Debug.Log("World pos of cell center "+ world2);
+            Debug.Log($"World: {world2} \nCell: {position}");
+            oldpos = position;
+        }
+    }
+}
