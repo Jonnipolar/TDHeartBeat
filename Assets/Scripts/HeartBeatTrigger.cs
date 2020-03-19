fcdc841883e9bfc0603dbc25df433bca9b87f064
ViktorSveins <viktorsveins@gmail.com>
2020-03-19 16:39:43 +0000

    HBTrigger: trigger heartbeat event every x seconds

---
+++ Assets/Scripts/HeartBeatTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HeartBeatTrigger : MonoBehaviour
+{    
+    public float startDelay = 0.0f;
+    public float beatInterval = 1.0f;
+
+    public UnityEvent HeartBeatEvent;
+
+    void Start()
+    {
+        if(HeartBeatEvent == null)
+        {
+            HeartBeatEvent = new UnityEvent();
+        }
+        StartCoroutine(BeatCoroutine());
+    }
+
+    IEnumerator BeatCoroutine()
+    {
+        yield return new WaitForSeconds(startDelay);
+        while(true)
+        {
+            yield return new WaitForSeconds(beatInterval);
+            EventManager.TriggerEvent("HeartBeat");
+        }
+    }
+    
+}
