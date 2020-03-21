2714bf30a97b2263117fffbdf71a7fee7200dbd5
ViktorSveins <viktorsveins@gmail.com>
2020-03-21 15:34:36 +0000

    Heart prefab, with animation

---
+++ Assets/Scripts/HeartBeatListen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBeatListen : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        EventManager.StartListening("HeartBeat", HandleHeartBeat);
+    }
+
+    void HandleHeartBeat()
+    {
+        anim.SetTrigger("Beat");
+    }
+}
